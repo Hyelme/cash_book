8276725e567a9b7b1f8613c325e42d0a32cf6df7
Hyelme <hyerim.ktds@gmail.com>
2024-01-15 21:45:25 +0900

    feat: 테이블 컴포넌트 생성

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,124 @@
-const Table = () => {
-  return <>Table</>;
+import { useTable } from "react-table";
+import "./Table.css";
+
+/**
+ * {
+  tableCN,
+  isThColspan,
+  isAllRowspan,
+  colItem,
+  rowItem,
+  rowSize,
+  colHeight,
+  rowRatio,
+}
+ */
+const useInstance = (instance) => {
+  const { allColumns } = instance;
+
+  let rowSpanHeaders = [];
+
+  allColumns.forEach((column, i) => {
+    const { id, enableRowSpan } = column;
+
+    if (enableRowSpan !== undefined) {
+      rowSpanHeaders = [
+        ...rowSpanHeaders,
+        { id, topCellValue: null, topCellIndex: 0 },
+      ];
+    }
+  });
+
+  Object.assign(instance, { rowSpanHeaders });
+};
+
+const Table = ({ classNameList, columns, data }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    rowSpanHeaders,
+  } = useTable({ columns, data }, (hooks) => {
+    // if (useInstance !== undefined) {
+    hooks.useInstance.push(useInstance);
+    // }
+  });
+
+  // console.log("Data : " + JSON.stringify(data, null, 2));
+
+  return (
+    <table className={classNameList.tableCN} {...getTableProps()}>
+      <thead>
+        {rowSpanHeaders.length === 0 &&
+          headerGroups.map((headerGroup) => (
+            <tr
+              className={classNameList.trCN}
+              {...headerGroup.getHeaderGroupProps()}
+            >
+              {headerGroup.headers.map((column) => {
+                return (
+                  <th
+                    className={classNameList.thCN}
+                    {...column.getHeaderProps()}
+                  >
+                    {column.render("Header")}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+
+          for (let j = 0; j < row.allCells.length; j++) {
+            let cell = row.allCells[j];
+            let rowSpanHeader = rowSpanHeaders.find(
+              (x) => x.id === cell.column.id
+            );
+
+            if (rowSpanHeader !== undefined) {
+              if (
+                rowSpanHeader.topCellValue === null ||
+                rowSpanHeader.topCellValue !== cell.value
+              ) {
+                cell.isRowSpanned = false;
+                rowSpanHeader.topCellValue = cell.value;
+                rowSpanHeader.topCellIndex = i;
+                cell.rowSpan = 1;
+              } else {
+                rows[rowSpanHeader.topCellIndex].allCells[j].rowSpan++;
+                cell.isRowSpanned = true;
+              }
+            }
+          }
+          return null;
+        })}
+        {rows.map((row) => {
+          return (
+            <tr className={classNameList.trCN} {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                if (cell.isRowSpanned) return null;
+                else
+                  return (
+                    <td
+                      className={classNameList.tdCN}
+                      rowSpan={cell.rowSpan}
+                      {...cell.getCellProps()}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
