a3ed17a1a13e96742110f4000411fd7fcf29cda3
Hyelme <hyerim.ktds@gmail.com>
2024-01-17 11:22:29 +0900

    design: 상단 월별 라벨 디자인

--- src/components/Label/MonthsLabel.jsx
+++ src/components/Label/MonthsLabel.jsx
@@ -1,16 +1,55 @@
+import styled from "styled-components";
 import "./MonthsLabel.css";
-import { useNavigate } from "react-router-dom";
 
-const MonthsLabel = ({ number }) => {
-  const navigate = useNavigate();
-  const handleOnClick = () => {
-    return navigate(`/cashbook/2024/monthly/${number}/calendar`);
+const MonthsLabel = ({ number, current, onClick }) => {
+  const handleOnClick = (event) => {
+    onClick(event);
   };
+
   return (
-    <div className="LabelComponent" onClick={handleOnClick}>
-      <div>{number}</div>
-    </div>
+    <LabelContainer>
+      {parseInt(current) !== number && (
+        <NonSelectLabel onClick={handleOnClick}>{number}</NonSelectLabel>
+      )}
+      {parseInt(current) === number && (
+        <SelecedLabel onClick={handleOnClick}>{number}</SelecedLabel>
+      )}
+    </LabelContainer>
   );
 };
 
+const LabelContainer = styled.div`
+  display: flex;
+  width: 11.1vw;
+`;
+const NonSelectLabel = styled.div`
+  width: 100%;
+  height: 50%;
+  background: #f2e5c7;
+  border-right: 2px solid #e9dbbb;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`;
+
+const SelecedLabel = styled.div`
+  width: 100%;
+  height: 65%;
+  background: #f2e5c7;
+  border-right: 2px solid #e9dbbb;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+
+  &:after {
+    content: "";
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 65%;
+    transform: translate(0, 40%);
+    background: #ffffffa8;
+    border-right: 2px solid #e9dbbb;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+`;
 export default MonthsLabel;
